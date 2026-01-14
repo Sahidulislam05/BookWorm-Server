1b696377eb412f06d8eff8faeb9f0727d6156391
Sahidulislam05 <sahidulislamcst@gmail.com>
2026-01-14 21:43:09 +0600

    Implement searching

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(
 // Enable CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
